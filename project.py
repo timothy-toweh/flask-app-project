a988589ef97be2ebddd2eafdc5d8ead42041477e
Timothy Toweh <136031096+timothy-toweh@users.noreply.github.com>
2024-08-24 13:06:07 +0100

    update

--- project.py
+++ project.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/") 
 def hello():
-    return "I am a Devops Engineer, TIMMY!"
+    return "I am a Devops Engineer!"
 
 if __name__ == "__main__": 
     app.run(host='0.0.0.0')
